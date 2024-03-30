6fc666fb83b61732e2386ddc92d80435b75eccef
Victor78970513 <yer29.chok@gmail.com>
2024-03-29 23:48:45 -0400

    database conection finished

--- apps/microservice2/src/microservice2.module.ts
+++ apps/microservice2/src/microservice2.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { Microservice2Controller } from './microservice2.controller';
 import { Microservice2Service } from './microservice2.service';
+import { TasksModule } from './tasks/tasks.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
+import { Task } from './tasks/task.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'tasks_db',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // entities: [__dirname + '/tasks/**/*.entity{.ts,.js}'],
+      entities: [Task],
+      synchronize: true
+    }),
+    TasksModule
+  ],
   controllers: [Microservice2Controller],
   providers: [Microservice2Service],
 })
--- apps/microservices-tancara/src/app.module.ts
+++ apps/microservices-tancara/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Microservice2Module } from 'apps/microservice2/src/microservice2.module';
 
 @Module({
-  imports: [],
+  imports: [
+    Microservice2Module,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
