25ce071d312bd398ace3d26d86592a9c8a7c76c5
nukkua <marco.salazar.2014.com@gmail.com>
2024-03-30 13:01:16 -0400

    formatted main m-service-1

--- apps/microservice1/src/microservice1.module.ts
+++ apps/microservice1/src/microservice1.module.ts
@@ -4,28 +4,28 @@ import { Microservice1Service } from './microservice1.service';
 import { PersonalModule } from './personal/personal.module';
 import { RolModule } from './rol/rol.module';
 import { PrioridadModule } from './prioridad/prioridad.module';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Personal } from './personal/entities/personal.entity';
 import { Rol } from './rol/entities/rol.entity';
 import { Prioridad } from './prioridad/entities/prioridad.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: 'localhost',
-      port: 3306,
-      username: 'chesco',
-      password: '1234',
-      database: 'nest_personal',
-      entities: [ Personal, Rol, Prioridad ],
-      synchronize: true,
-    }), 
-    PersonalModule,
-    RolModule,
-    PrioridadModule
-  ],
-  controllers: [Microservice1Controller],
-  providers: [Microservice1Service],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'mariadb',
+			host: 'localhost',
+			port: 3306,
+			username: 'chesco',
+			password: '1234',
+			database: 'nest_personal',
+			entities: [Personal, Rol, Prioridad],
+			synchronize: true,
+		}),
+		PersonalModule,
+		RolModule,
+		PrioridadModule
+	],
+	controllers: [Microservice1Controller],
+	providers: [Microservice1Service],
 })
-export class Microservice1Module {}
+export class Microservice1Module { }
