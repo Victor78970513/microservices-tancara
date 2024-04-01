b16e733c7f56af3b2205bcf2d740cef3090b0071
nukkua <marco.salazar.2014.com@gmail.com>
2024-03-31 22:34:08 -0400

    done

--- apps/microservice2/src/microservice2.module.ts
+++ apps/microservice2/src/microservice2.module.ts
@@ -1,26 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import {TypeOrmModule} from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { Task } from './tasks/task.entity';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
 
-    ConfigModule.forRoot(),
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: 'localhost',
-      port: 3306,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: 'tasks_db',
-      entities: [Task],
-      synchronize: true
-    }),
-    TasksModule
-  ],
-  controllers: [],
-  providers: [],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: 'mariadb',
+			host: 'localhost',
+			port: 3306,
+			username: process.env.DATABASE_USER,
+			password: process.env.DATABASE_PASSWORD,
+			database: 'tasks_db',
+			entities: [Task],
+			synchronize: true
+		}),
+		TasksModule
+	],
+	controllers: [],
+	providers: [],
 })
-export class Microservice2Module {}
+export class Microservice2Module { }
