51f084ca2ac1441472947abbb22a4ea8c3dc97ff
nukkua <marco.salazar.2014.com@gmail.com>
2024-03-31 20:53:35 -0400

    ala

--- apps/microservice1/src/main.ts
+++ apps/microservice1/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { Microservice1Module } from './microservice1.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(Microservice1Module);
-  await app.listen(3000);
+	const app = await NestFactory.create(Microservice1Module);
+	app.enableCors();
+	await app.listen(3002);
 }
 bootstrap();
